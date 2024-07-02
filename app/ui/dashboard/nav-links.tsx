33683d41d48d3e2c1b4d7a13de30ae9a2648f1ad
laazar26 <116655862+laazar26@users.noreply.github.com>
2024-07-02 16:48:56 +0200

    fixed error in nav

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,16 +1,14 @@
-'use client'
-
+'use client';
+ 
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+import clsx from 'clsx';
+ 
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
